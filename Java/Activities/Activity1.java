96a87734321e82c20f547788c2df120e14a089d2
Pranavi1997 <kandulapranavi69@gmail.com>
2025-12-02 16:10:24 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,17 @@
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1=new Car();	
+		car1.color = "Black";
+		car1.make = 2014;
+		car1.transmission = "Manual";
+		car1.displayCharacteristics();
+		car1.accelerate();
+		car1.brake();
+		
+				
+	}
+
+}
