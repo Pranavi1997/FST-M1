28159e4c21cac25441f891bb7b08800552060f84
Pranavi1997 <kandulapranavi69@gmail.com>
2025-12-30 16:55:37 +0530

    Add files via upload

--- Appium/Project/Activity2.java
+++ Appium/Project/Activity2.java
@@ -1,21 +1,24 @@
-package googleChrome;
+package nativeApp;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.time.Duration;
-import java.util.List;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
@@ -29,75 +32,39 @@ public class Activity2 {
 	
 	@BeforeClass
 	public void setup() throws MalformedURLException, URISyntaxException {
-		UiAutomator2Options options = new UiAutomator2Options();
-		options.setPlatformName("android");
-		options.setAutomationName("UiAutomator2");
-		options.setAppPackage("com.android.chrome");
-		options.setAppActivity("com.google.android.apps.chrome.Main");
-		options.noReset();
-		URL serverURL = new URI("http://localhost:4723").toURL();
-		driver = new AndroidDriver(serverURL, options);
+		UiAutomator2Options caps=new UiAutomator2Options();
+		File appFile=new File("src/test/resources/ts-todo-list-v1.apk");
+		caps.setPlatformName("android");
+		caps.setAutomationName("UiAutomator2");
+		caps.setApp(appFile.getAbsolutePath());
+		caps.noReset();
+		URL serverurl=new URI("http://localhost:4723").toURL();
+		driver = new AndroidDriver(serverurl,caps);	
 		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-		driver.get("https://training-support.net/webelements");
 	}
 	
-	@Test(priority = 1)
-	public void validTest() {
-		wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.className("android.view.View")));
-		List<WebElement> textElements = driver.findElements(AppiumBy.className("android.view.View"));
-		String UiScrollable = "UiScrollable(UiSelector().scrollable(true))";
-		wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.androidUIAutomator(UiScrollable + ".scrollForward(65)")));
-		WebElement text = driver.findElement(AppiumBy
-				.androidUIAutomator(UiScrollable + ".scrollTextIntoView(\"Login Form\")"));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text=\"Login Form\"]")));
-	  driver.findElement(AppiumBy.xpath(
-			  "//android.widget.TextView[@text=\"Login Form\"]")).click();	 
-	  wait.until(ExpectedConditions.elementToBeClickable(
-              AppiumBy.xpath("//android.widget.EditText[@resource-id=\"username\"]")));
-	  driver.findElement(AppiumBy.xpath(
-			  "//android.widget.EditText[@resource-id=\"username\"]"
-			  )).sendKeys("admin");
-	  driver.findElement(AppiumBy.xpath(
-			  "//android.widget.EditText[@resource-id=\"password\"]"
-			  )).sendKeys("password");
-	  driver.findElement(AppiumBy.xpath(
-			  "//android.widget.Button[@text=\"Submit\"]")).click();
-	  wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.xpath(
-			  "//android.widget.TextView[@text=\"Login Success!\"]")));
-	  String messageText = driver.findElement(AppiumBy.xpath(
-				"//android.widget.TextView[@text=\"Login Success!\"]")).getText();		
-		System.out.println(messageText);
-		assertEquals(messageText,"Login Success!");
-	}
-	
-	@Test(priority = 2)
-	public void invalidTest() {
-		driver.get("https://training-support.net/webelements");
-		wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.className("android.view.View")));
-		List<WebElement> textElements = driver.findElements(AppiumBy.className("android.view.View"));
-		String UiScrollable = "UiScrollable(UiSelector().scrollable(true))";
-		wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.androidUIAutomator(UiScrollable + ".scrollForward(65)")));
-		WebElement text = driver.findElement(AppiumBy
-				.androidUIAutomator(UiScrollable + ".scrollTextIntoView(\"Login Form\")"));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text=\"Login Form\"]")));
-	  driver.findElement(AppiumBy.xpath(
-			  "//android.widget.TextView[@text=\"Login Form\"]")).click();	 
-	  wait.until(ExpectedConditions.elementToBeClickable(
-              AppiumBy.xpath("//android.widget.EditText[@resource-id=\"username\"]")));
-	  driver.findElement(AppiumBy.xpath(
-			  "//android.widget.EditText[@resource-id=\"username\"]"
-			  )).sendKeys("admin");
-	  driver.findElement(AppiumBy.xpath(
-			  "//android.widget.EditText[@resource-id=\"password\"]"
-			  )).sendKeys("password1");
-	  driver.findElement(AppiumBy.xpath(
-			  "//android.widget.Button[@text=\"Submit\"]")).click();
-	  wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.xpath(
-			  "//android.widget.TextView[@text=\"Invalid credentials\"]")));
-	  String messageText = driver.findElement(AppiumBy.xpath(
-				"//android.widget.TextView[@text=\"Invalid credentials\"]")).getText();		
-		System.out.println(messageText);
-		assertEquals(messageText,"Invalid credentials");
+	@Test
+	public void todolistTest1() {
+		driver.findElement(AppiumBy.accessibilityId("Predicted app: To-Do List")).click();	
+		WebElement firsttask = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.androidUIAutomator("text(\"Activity 1\")")));
+		((JavascriptExecutor) driver).executeScript("mobile: longClickGesture", ImmutableMap.of("elementId", ((RemoteWebElement) firsttask).getId(), "duration", 2000));		
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@resource-id=\"android:id/title\" and @text=\"Edit To-Do Task\"]")).click();
+		wait.until(ExpectedConditions.elementToBeClickable(
+				AppiumBy.id("tv_todo_list_deadline")));
+		driver.findElement(AppiumBy.id("tv_todo_list_deadline")).click();
+		driver.findElement(AppiumBy.accessibilityId("Next month")).click();
+		driver.findElement(AppiumBy.xpath("//android.view.View[@content-desc=\"10 January 2026\"]")).click();
+		driver.findElement(AppiumBy.id("bt_deadline_ok")).click();
+		wait.until(ExpectedConditions.elementToBeClickable(
+				AppiumBy.id("bt_new_task_ok")));
+		driver.findElement(AppiumBy.id("bt_new_task_ok")).click();
+		wait.until(ExpectedConditions.elementToBeClickable(
+				AppiumBy.id("tv_exlv_task_deadline")));
+		String result1 = driver.findElement(AppiumBy.xpath(
+				"//android.widget.TextView[contains(@text,'Deadline: 10.01.2026')]"
+				)).getText();
+		System.out.println(result1);
+		assertEquals(result1,"Deadline: 10.01.2026");
 	}
 	@AfterClass
 	public void tearDown() {
